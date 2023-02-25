b09730b92a7c8b9ae81f8ba15440b538ef96b047
Rajtilak patel <75354435+rajtilak-patel@users.noreply.github.com>
2023-02-25 11:35:43 +0530

    react-router-dom update (#1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Cart from "./Cart";
+import Contact from "./Contact";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+
+import Products from "./Pages/Products";
+import SingleProduct from "./SingleProduct";
+
+
 
 const App = () => {
-  return <div>Welcome to Raj React E-Commerce Website</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+           <Route path="/" element={<Home/>} />
+           <Route path="/about" element={<About/>} />
+           <Route path="/contact" element={<Contact/>} />
+           <Route path="/product" element={<Products/>} />
+           <Route path="/singleproduct/:id" element={<SingleProduct/>} />
+           <Route path="/cart" element={<Cart/>} />
+        </Routes>
+        
+
+      </Router>
+    
+    </>
+  )
+  
 };
 
 export default App;
