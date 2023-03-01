eea724512c61eaf3448cab550bd4c16e38005d4c
Rajtilak patel <75354435+rajtilak-patel@users.noreply.github.com>
2023-03-01 12:55:26 +0530

    added (#4)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import About from "./About";
 import Home from "./Home";
-// import Products from "./Products";
-// import Contact from "./Contact";
-// import Cart from "./Cart";
-// import SingleProduct from "./SingleProduct";
-// import ErrorPage from "./ErrorPage";
+import Products from "./Products";
+import Contact from "./Contact";
+import Cart from "./Cart";
+import SingleProduct from "./SingleProduct";
+import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import About from "./About";
@@ -27,11 +27,11 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/products" element={<Products />} /> */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
-          {/* <Route path="/singleproduct/:id" element={<SingleProduct />} /> */}
-          {/* <Route path="/cart" element={<Cart />} /> */}
-          {/* <Route path="*" element={<ErrorPage />} /> */}
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/singleproduct/:id" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
     </ThemeProvider>
